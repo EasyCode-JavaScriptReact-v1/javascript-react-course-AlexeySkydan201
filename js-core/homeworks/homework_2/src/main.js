d275f50a829580f9f353ca2e1b6b92a0bc80ef6e
alexey201 <notyandex201@gmail.com>
2018-05-31 22:13:10 +0300

    2

---
+++ js-core/homeworks/homework_2/src/main.js
@@ -0,0 +1,24 @@
+var user = {
+    name:['firstName','secondName','nickName'],
+    placeOfWork:{
+    	company:null,
+    	position:null
+    },
+    phone:{
+    	mobile:['111111','222222'],
+    	home:['333333'],
+    	work:[,'444444']
+    },
+    email: undefined,
+    chat: null,
+    location:{
+    	city: 'Kharkiv',
+    	street:null,
+    	home: null,
+    	apartment:201
+    },
+   rington:[true,false],
+};
+console.log(user);
+console.log(typeof user.phone.work[0]);
+//2
