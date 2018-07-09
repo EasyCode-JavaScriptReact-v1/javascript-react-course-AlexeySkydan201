964e5a759ee0e98799432b56beacb30b3dccec05
alexey201 <notyandex201@gmail.com>
2018-07-09 22:53:21 +0300

    home work 12

---
+++ js-core/homeworks/homework-12/src/main.js
@@ -0,0 +1,98 @@
+﻿/* Task 0
+ * Создайте функцию конструктор Http, которая будет иметь 2 метода
+ * createServer() - принимает один аргумент функцию с двумя параметрами ctx и next
+ * ctx: Object {
+ *   req: Object
+ *   PORT: number
+ *   url: string
+ *   res: Object
+ *   status: number,
+ *   message: string,
+ *   header: Object {
+ *      content-type:application/json
+ *   }
+ * }
+ * next: Function
+ * при вызове listen(PORT, host) - в консоле должна отобразится надпись
+ * "Server running on https://host:port"
+ * и вызваться переданная в createServer функция
+ * методы нужно вызывать через chain
+ * после вызова метода listen() - должна вызываться переданная в createServer
+ * первая функция и возвращать объект и функцию
+ */
+var ctx = {
+    req: {
+        PORT: 'number',
+        url: 'string',
+    },
+    res: {
+        status: 'number',
+        message: 'string',
+        header: {
+            content_type: 'application / json'
+        }
+    }
+}
+var next = function() { console.log(0); };
+
+function Http() {}
+Http.prototype.createServer = function(fn) {
+    this.fn = fn; //function(ctx, next) { console.log(ctx); }
+    return this;
+}
+Http.prototype.listen = function(PORT, host) {
+    console.log(`"Server running on https://${host}:${PORT}"`);
+    this.fn(ctx, next);
+
+}
+const server = new Http().createServer(function(ctx, next) {
+    console.log(ctx);
+}).listen(3000, 'localhost');
+
+// TASK 1
+// Создать класс Human, у которого будут свойства обычного человека:
+// имя, возраст, пол, рост, вес.
+// Используя прототипное наследование создать дочерние классы Worker
+// (дописать в них поля места работы, зарплата, метод "работать")
+// и Student (дописать поля места учебы, стипендией, метод "смотреть сериалы")
+//
+// Создать несколько экземпляров классов Worker и Student, вывести их в консоль.
+// Убедиться что они имеют поля родительского класса Human
+
+var Human = function(name, age, pol, height, weight) {
+    this.name = name;
+    this.age = age;
+    this.pol = pol;
+    this.height = height;
+    this.weight = weight;
+}
+var Worker = function(name, age, pol, height, weight) {
+    this.placeOfWork = 'stok';
+    this.salary = 6500;
+    Human.call(this, name, age, pol, height, weight);
+    this.work4 = () => {
+        console.log(`работать`);
+    }
+}
+var Student = function(name, age, pol, height, weight) {
+    this.colledge = 'easy';
+    this.stipendia = '300$';
+    Human.call(this, name, age, pol, height, weight);
+    this.visionTV = () => {
+        console.log(`смотреть сериалы`);
+    }
+}
+
+Worker.prototype = new Human();
+Student.prototype = new Human();
+
+var alexWorker = new Worker('alex', 38, 'm', 180, 110);
+var vasyaWorker = new Worker('vasya', 47, 'm', 170, 120);
+
+var alfredStudent = new Student('alfred', 47, 'm', 197, 80);
+var annaStudent = new Student('anna', 18, 'w', 190, 62);
+
+console.log(alexWorker);
+console.log(vasyaWorker);
+console.log(alfredStudent);
+console.log(annaStudent);
