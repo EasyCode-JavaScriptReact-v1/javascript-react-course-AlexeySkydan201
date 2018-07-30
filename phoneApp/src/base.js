9dd3f9eca03fefe7a917bb52aad1dad9e9015066
alexey201 <notyandex201@gmail.com>
2018-07-30 23:40:52 +0300

    phone book, contact and keypad

---
+++ phoneApp/src/base.js
@@ -0,0 +1,37 @@
+var bd = [{
+        name: 'alex1',
+        lastName: 'Sky11',
+        mail: 'yu@yy14.com'
+    },
+    {
+        name: 'balex2',
+        lastName: 'Sky22',
+        mail: 'yy@yy2.com'
+    },
+    {
+        name: 'calex3',
+        lastName: 'Sky31',
+        mail: 'yt@yy3.com'
+    },
+    {
+        name: 'dalex4',
+        lastName: 'Sky2',
+        mail: 'yy@ky26.com'
+    },
+    {
+        name: 'balex7',
+        lastName: 'Sky43',
+        mail: 'ay@yy3.com'
+    },
+    {
+        name: 'calex42',
+        lastName: 'Sky2',
+        mail: 'cy@yy2.com'
+    },
+    {
+        name: 'aalex13',
+        lastName: 'Sky3',
+        mail: 'by@yy3.com'
+    }
+
+]
