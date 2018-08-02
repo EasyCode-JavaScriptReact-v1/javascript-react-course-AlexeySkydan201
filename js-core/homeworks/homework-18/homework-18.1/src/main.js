5560b10c03eb997783154f029544ecaa466812c4
alexey201 <notyandex201@gmail.com>
2018-08-02 23:17:04 +0300

    ресайз недополучился

---
+++ js-core/homeworks/homework-18/homework-18.1/src/main.js
@@ -0,0 +1,25 @@
+﻿/* EASY
+Алгоритмы !
+TASK 0
+Вам дано предложение, верните массив из слов,
+которые длинее чем средняя длина всех слов.
+Слова разделены пробелами, если в предложении запятые,они должны быть пропущены
+solution(["This is a sample string"]) expected ["This" "sample" "string"]
+solution(["Some another sample"]) expected ["another" "sample"]
+solution(["Do, do, do, do... do it!"]) expected []
+*/
+var solution = (arr) => {
+    var str = arr[0];
+    str = str.replace(/[,.!]/gmi, '').split(' ');
+    var result = str.reduce((sum, value) => {
+        return sum = sum + value.length;
+    }, 0);
+    var midle = result / str.length;
+    var pos = str.filter((value) => {
+        return value.length > midle;
+    });
+    console.log(pos);
+}
+solution(["This is a sample string"]);
+solution(["Some another sample"]);
+solution(["Do, do, do, do... do it!"]);
---
+++ js-core/homeworks/homework-18/homework-18.3/src/main.js
@@ -0,0 +1,51 @@
+﻿var resizes = document.getElementsByClassName('resize');
+var sections = document.querySelector('section');
+console.log(sections);
+
+[...sections.children].forEach((section, i) => {
+    let resize = section.children[0];
+    console.log(section);
+    resize.onmousedown = (e) => {
+        var coords = getCoords(resize);
+        var shiftX = e.pageX;
+        //var shiftY = e.pageY - coords.top;
+        var sectionCoords = getCoords(section);
+
+        console.log(`s `, sectionCoords, `q `, e.pageX);
+        //moveAt(e);
+        //resize.style.zIndex = 1000;
+
+        function moveAt(e) {
+
+            resize.style.left = (e.pageX - shiftX) + 'px';
+            console.log(`e `, section.style.width);
+            section.style.width = (e.pageX - shiftX) + 'px';
+
+            //console.log(e);
+            //resize.style.top = e.pageY - shiftY + 'px';
+        }
+        document.onmousemove = function(e) {
+            moveAt(e);
+        };
+
+        resize.onmouseup = function() {
+            document.onmousemove = null;
+            resize.onmouseup = null;
+            section.onmouseup = null;
+        };
+        resize.ondragstart = function() {
+            return false;
+        };
+
+    }
+});
+
+function getCoords(elem) { // кроме IE8-
+    var box = elem.getBoundingClientRect();
+
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+
+}
